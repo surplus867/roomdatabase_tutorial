816c9e13f4e1cb85431ec4a7da1e2bb191ac4454
billyyan <By869422>
2023-12-09 22:39:34 -0800

    Notes App with Room DB

---
+++ app/src/main/java/com/example/roomdatabase/presentation/NoteState.kt
@@ -0,0 +1,12 @@
+package com.example.roomdatabase.presentation
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import com.example.roomdatabase.data.Note
+
+data class NoteState(
+
+    val notes: List<Note> = emptyList(),
+    val title: MutableState<String> = mutableStateOf(""),
+    val description: MutableState<String> = mutableStateOf("")
+)
